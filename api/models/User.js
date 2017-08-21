bb7dde26fce8804a88d0ba5dcc32a2cf619ff98a
Sean Lantz <slantzjr@Seans-Air.austin.rr.com>
2017-08-20 19:54:06 -0500

    Create admin

--- api/models/User.js
+++ api/models/User.js
@@ -33,7 +33,6 @@ module.exports = {
     toJSON: function() {
       var obj = this.toObject();
       delete obj.password;
-      delete obj.confirmation;
       delete obj.encryptedPassword;
       return obj;
     }
