10f1896f1dbc23a6ed72bb2217ab2be17021e0e4
Sean Lantz <slantzjr@gmail.com>
2017-09-16 19:38:12 -0500

    Add graph for light intensity

--- api/controllers/MeasurementController.js
+++ api/controllers/MeasurementController.js
@@ -8,14 +8,18 @@
 module.exports = {
 	create: function(req, res) {
     var data = {}
-    if (_.isUndefined(req.param('temperature'))) {
-      return res.badRequest('A temperature measurement is required!');
-    }
     if (_.isUndefined(req.param('hasWater'))) {
       return res.badRequest('hasWater is required');
     }
-    data.temperature = req.param('temperature');
+    if (_.isUndefined(req.param('lightIntensity'))) {
+      return res.badRequest('lightIntensity is required');
+    }
+    if (_.isUndefined(req.param('temperature'))) {
+      return res.badRequest('A temperature measurement is required!');
+    }
     data.hasWater = req.param('hasWater');
+    data.lightIntensity = req.param('lightIntensity');
+    data.temperature = req.param('temperature');
     Measurement.create(data).exec(function(err, createdMeasurement) {
       if (err) {
         return res.negotiate(err);
--- assets/js/controllers/measurementHistoryController.js
+++ assets/js/controllers/measurementHistoryController.js
@@ -2,7 +2,7 @@ angular.module('gardenMonitor').controller('measurementHistoryController', ['$lo
   // Set up $scope.measurements as an empty array to ensure it always exists.
   $scope.measurements = [];
 
-  $scope.options = {
+  $scope.tempOptions = {
     "chart": {
       "type": "lineChart",
       "height": 450,
@@ -37,22 +37,75 @@ angular.module('gardenMonitor').controller('measurementHistoryController', ['$lo
     },
   };
 
+  $scope.lightOptions = {
+    "chart": {
+      "type": "lineChart",
+      "height": 450,
+      "margin": {
+        "top": 20,
+        "right": 20,
+        "bottom": 40,
+        "left": 55
+      },
+      "useInteractiveGuideline": true,
+      "dispatch": {},
+      "xAxis": {
+        "axisLabel": "Time",
+        tickFormat: function(d) { return d3.time.format('%b %d')(new Date(d)); }
+      },
+      "yAxis": {
+        "axisLabel": "Light Intensity (lx)",
+        "axisLabelDistance": -10
+      }
+    },
+    "title": {
+      "enable": false,
+      "text": ""
+    },
+    "subtitle": {
+      "enable": true,
+      "text": "Last 5 days",
+      "css": {
+        "text-align": "center",
+        "margin": ""
+      }
+    },
+  };
+
   // First, show a loading spinner
   $scope.measurementsLoading = true;
   // Get the existing measurements.
   $http({method: 'GET', url: '/measurement'}).then(function whenServerResponds(response) {
     $scope.measurementsLoading = false;
     $scope.hasMeasurements = response.data.length > 0;
-    var preppedData = [];
+    var preppedTempData = [];
+    var preppedLightData = [];
     if (response.data) {
-      var preppedData = response.data.map(
+      preppedTempData = response.data.map(
         function(point) {
-          return {x: new Date(point.createdAt).getTime(), y: point.temperature};
+          return {x: new Date(point.createdAt).getTime(), y: parseFloat(point.temperature)};
         }
       );
-      $scope.data = [{
+      preppedLightData = response.data.map(
+        function(point) {
+          if (!point.lightIntensity) {
+            return {x: new Date(point.createdAt).getTime(), y: null};
+          }
+          return {x: new Date(point.createdAt).getTime(), y: parseFloat(point.lightIntensity)};
+        }
+      );
+      preppedLightData = preppedLightData.filter(
+        function(point) {
+          return point.y != null;
+        }
+      );
+      $scope.tempData = [{
         key: "Temperature",
-        values: preppedData
+        values: preppedTempData
+      }];
+      $scope.lightData = [{
+        key: "Light Intensity",
+        values: preppedLightData
       }];
     }
   });
